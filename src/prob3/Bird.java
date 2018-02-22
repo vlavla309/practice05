60e0996a823a9276dcb6a24f0db1e497f79435da
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-22 17:10:00 +0900

    prob3 Bird

--- src/prob3/Bird.java
+++ src/prob3/Bird.java
@@ -1,4 +1,19 @@
 package prob3;
 
 public abstract class Bird {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public abstract void fly();
+	
+	public abstract void sing();
+	
+	public abstract String toString();
 }

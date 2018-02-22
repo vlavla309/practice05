962f0d66021a43adfde73c78190a6f0963c934cf
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-22 16:57:02 +0900

    prob2 스마트폰

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -2,4 +2,23 @@ package prob2;
 
 public class SmartPhone extends MusicPhone {
 
+	@Override
+	public void execute(String function) {
+		if(function.equals("앱")) {
+			excuteApp();
+			return;
+		}
+		
+		super.execute(function);
+	}
+	
+	@Override
+	protected void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+
+	protected void excuteApp() {
+		System.out.println("앱실행");
+	}
+	
 }
